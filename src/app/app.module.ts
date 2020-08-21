a67211a4d78cef98ac45b28624bd3d59e970fef0
animamundi <contact@nathan-mcgrath.com>
2020-08-21 14:34:52 +0200

    1 file modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
-import { FormControlOutletComponent } from "./form-control-outlet.directive";
+import { FormControlOutletComponent } from "./form-control-outlet.component";
 import { CustomInputComponent } from "./custom-input.component";
 
 @NgModule({
